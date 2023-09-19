04f11a85ebf9574b7737b0e48859ffd066d2f736
Nidhal <nidhal@N2SY.local>
2023-09-19 11:21:43 +0100

    add GET, POST, GET by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,51 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller('tasks')
 export class TasksController {
+  tabTasks = [];
   @Get('all')
   getAllTasks(@Req() request: Request, @Res() response: Response) {
     console.log(request);
-    return response.status(200).json({ alltasks: [] });
+    return response.status(200).json({ alltasks: this.tabTasks });
+  }
+
+  //   @Get('all/:id/:prenom/:age')
+  //   getTaskById(@Param('id') i, @Param('age') a, @Res() response: Response) {
+  //     console.log(i, a);
+  //   }
+
+  @Get('all/:id')
+  getTaskById(@Param('id') taskId, @Res() response: Response) {
+    let selectedTask = this.tabTasks.find((t) => t.id == taskId);
+    if (selectedTask) {
+      return response.status(200).json({ result: selectedTask });
+    } else throw new NotFoundException("Le task demandé n'existe pas");
   }
 
   @Post('new')
-  addNewTask(@Body() newTask) {
-    console.log(newTask);
+  addNewTask(@Body() newTask, @Res() response: Response) {
+    if (!this.tabTasks.length)
+      this.tabTasks.push({
+        id: 1,
+        ...newTask,
+      });
+    else {
+      this.tabTasks.push({
+        id: this.tabTasks[this.tabTasks.length - 1].id + 1,
+        ...newTask,
+      });
+    }
+    return response.status(201).json({ message: 'Ajout réussi du task' });
   }
 
   @Post('new1')
